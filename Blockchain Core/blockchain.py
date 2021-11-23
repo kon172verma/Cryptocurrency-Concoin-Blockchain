f16e78a4a1a5627284cc7f61c34d50afb8f01027
Konark Verma <kon172verma@gmail.com>
2021-11-23 08:53:20 +0530

    Added comments

--- Blockchain Core/blockchain.py
+++ Blockchain Core/blockchain.py
@@ -1,7 +1,14 @@
+# Importing the required libraries.
 import datetime
 import hashlib
 import json
+
+
+# Defined class for a block.
 class Block:
+    # A block has the following values: index(block number), timestamp(time of block creation), 
+    # nonce(number to be used once for PoW), data(information that needs to be stored on the blockchain),
+    # prevHash(hash of the previous block), hash(hash value of the current block).
     def __init__(self, index, data, prevHash) -> None:
         self.index = index
         self.timestamp = int(datetime.datetime.now().timestamp())
@@ -10,22 +17,30 @@ class Block:
         self.prevHash = prevHash
         self.hash = 'YTD'
 
+
+# Defined class for blockchain and its required operations.
 class Blockchain:
+    # Initialized an empty list for blockchain.
     def __init__(self) -> None:
         self.chain = []
 
+    # Function to print all the blocks in our blockchain.
     def printchain(self) -> None:
         for i in self.chain:
             print(i.index, i.timestamp, i.data, i.nonce, i.prevHash, i.hash)
 
+    # Function to fetch the hash of the last block in our blockchain.
     def prevHash(self):
         if len(self.chain) == 0:
             return 0
         return self.chain[-1].hash
 
+    # Function to create a new object of the class block.
     def createBlock(self, data):
         return Block(len(self.chain), data, self.prevHash())
 
+    # Function to compute the Proof of Work(PoW): nonce and the hash value.
+    # Here we have used SHA-256, and the target is the hash value with '0000' as prefix.
     def pow(self, block) -> None:
         jsonData = {
             'index': block.index,
@@ -43,12 +58,14 @@ class Blockchain:
             nonce += 1
         return nonce, value
 
+    # Function to add a new block to our blockchain.
     def addBlock(self):
         data = 'some data for now'
         block = self.createBlock(data)
         block.nonce, block.hash = self.pow(block)
         self.chain.append(block)
 
+# Test code.
 chain = Blockchain()
 chain.addBlock()
 chain.addBlock()
