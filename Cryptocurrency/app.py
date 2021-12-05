6c062a98b45bbc9e0ba96be1d3943cf6bc7f5161
Konark Verma <kon172verma@gmail.com>
2021-12-05 14:35:43 +0530

    created flask app for concoin

---
+++ Cryptocurrency/app.py
@@ -0,0 +1,40 @@
+import json
+from concoin import Concoin
+from flask import Flask
+from flask.templating import render_template
+import json
+import random
+import string
+
+
+app = Flask(__name__)
+concoin = Concoin()
+
+@app.route('/', methods=['GET'])
+def home():
+    response = json.dumps(concoin.chain, indent = 4, separators = (',', ': '))
+    return render_template( 'results.html', response = response ), 200
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    result = concoin.mine_block()
+    response = 'New block mined successfully' if result else 'Not enough transactions.'
+    return response, 200
+
+@app.route('/add_random_transactions', methods=['GET'])
+def add_random_transactions():
+    for i in range(13):
+        sender = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+        receiver = ''.join(random.choices(string.ascii_uppercase + string.digits, k=7))
+        amount = random.randint(1,1000)
+        concoin.add_transaction(sender, receiver, amount)
+    response = json.dumps(concoin.transactions_pool, indent = 4, separators = (',', ': '))
+    return render_template( 'results.html', response = response ), 200
+
+@app.route('/verify_chain', methods=['GET'])
+def verify_chain():
+    result = concoin.verify_chain()
+    response = 'Chain verified. Everything is okay.' if result else 'Chain corrupted.'
+    return response, 200
+
+app.run('localhost', 5000)
