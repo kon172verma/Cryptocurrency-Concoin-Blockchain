70535bd3850c145ac2591ac1aa2785ef8d9a8880
Konark Verma <kon172verma@gmail.com>
2021-12-05 14:38:42 +0530

    Added comments - app.py

--- Cryptocurrency/app.py
+++ Cryptocurrency/app.py
@@ -1,4 +1,4 @@
-import json
+# Importing the required libraries and modules.
 from concoin import Concoin
 from flask import Flask
 from flask.templating import render_template
@@ -6,21 +6,26 @@ import json
 import random
 import string
 
-
+# Initializing our app.
 app = Flask(__name__)
+
+# Initializing the blockchain for cryptocurrency-concoin.
 concoin = Concoin()
 
+# Route to print all the blocks in our blockchain.
 @app.route('/', methods=['GET'])
 def home():
     response = json.dumps(concoin.chain, indent = 4, separators = (',', ': '))
     return render_template( 'results.html', response = response ), 200
 
+# Route to mine a new block.
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
     result = concoin.mine_block()
     response = 'New block mined successfully' if result else 'Not enough transactions.'
     return response, 200
 
+# Route to add a bunch of random transactions.
 @app.route('/add_random_transactions', methods=['GET'])
 def add_random_transactions():
     for i in range(13):
@@ -31,10 +36,12 @@ def add_random_transactions():
     response = json.dumps(concoin.transactions_pool, indent = 4, separators = (',', ': '))
     return render_template( 'results.html', response = response ), 200
 
+# Route to verify the entire blockchain.
 @app.route('/verify_chain', methods=['GET'])
 def verify_chain():
     result = concoin.verify_chain()
     response = 'Chain verified. Everything is okay.' if result else 'Chain corrupted.'
     return response, 200
 
+# Running our flask app.
 app.run('localhost', 5000)
